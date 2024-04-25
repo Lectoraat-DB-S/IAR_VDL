be631d219b0006c43c86d8e8fc71605a81927b0c
Nols <91676490+NielsGoBrrr@users.noreply.github.com>
2024-04-25 11:23:02 +0200

    Saving files attempt #1

--- Kalibratie/Main.py
+++ Kalibratie/Main.py
@@ -1,10 +1,15 @@
 import URCommunication as urc
 import time
+import csv
+import os
 
 # Per connection specifics.
 IP_UR = "192.168.0.13"
 PORT_RECIEVE = 30022
 
+TOOL_IN = 0
+TOOL_FLUSH = 13
+TOOL_OUT = 55
 
 # Functions
 def MeasurePoint(write_conn,read_conn):
@@ -15,8 +20,16 @@ def MeasurePoint(write_conn,read_conn):
         print("Waiting...")
     print("Done")
     time.sleep(0.5)
-    print("[COBOT] Position is: " + urc.readWrite("get_actual_tcp_pose()",write_conn,read_conn))
+    measuredPoint = urc.readWrite("get_actual_tcp_pose()",write_conn,read_conn)
+    print("[COBOT] Position is: " + measuredPoint)
     urc.syncWrite("movel(" + curPose + ",a=1.2,v=0.25,t=0,r=0)",write_conn,read_conn)
+
+    try:
+        with open("points.csv", newline = '') as f:
+            writer = csv.writer(f)
+            writer.writerow(["Point 1",measuredPoint])
+    except Exception as err:
+        print(err)
 
 # Script part.
 urc.greenLightUR(IP_UR)
@@ -24,7 +37,9 @@ write,read = urc.connectReadWrite(IP_UR,PORT_RECIEVE)
 
 starting_position = "p[-0.359385,0.175107,0.178234,0.00322613,3.14051,0.00389871]"
 
-urc.syncWrite("movej(" + starting_position +")",write,read)
+#urc.syncWrite("movej(" + starting_position +")",write,read)
+urc.asyncWrite("sd_move(" + str(TOOL_IN) + ",0)",write)
 MeasurePoint(write,read)
+urc.asyncWrite("sd_move(" + str(TOOL_FLUSH) + ",0)",write)
 
 urc.closeReadWrite(write,read)
