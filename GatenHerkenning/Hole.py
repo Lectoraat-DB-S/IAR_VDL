807a4b6c6487e8855a3fb62a8cac2a7ab0db901a
DAStitselaar <99558089+DAStitselaar@users.noreply.github.com>
2024-05-21 22:13:52 +0200

    -Huidige situatie niet werkend

--- GatenHerkenning/Hole.py
+++
@@ -1,6 +0,0 @@
-class Hole:
-    def __init__(self, location, direction, diameter, depth):
-        self.location = location
-        self.direction = direction
-        self.diameter = diameter
-        self.depth = depth
