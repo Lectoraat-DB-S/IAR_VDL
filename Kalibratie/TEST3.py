15807d9625e1d6aa4bc5a59a89ca793f6d991fb5
Nols <91676490+NielsGoBrrr@users.noreply.github.com>
2024-06-04 13:33:53 +0200

    Figured out formula extraction via lstsq.
    See TEST3.py

--- Kalibratie/TEST3.py
+++ Kalibratie/TEST3.py
@@ -1,1 +1,12 @@
+import numpy as np
+import math as mt
+
 # Insert measured points into Least squares method.
+DataPoints_x = [0,1,2,3]
+DataPoints_y = [1,2,3,4]
+
+A = np.vstack([DataPoints_x,np.ones(len(DataPoints_x))]).T
+print(A)
+m,c = np.linalg.lstsq(A,DataPoints_y,rcond=None)[0]
+print(round(m,10))
+print(round(c,10))
