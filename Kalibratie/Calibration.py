c762d6bf7031d4db910788e83494bc021fe3ec33
Nols <91676490+NielsGoBrrr@users.noreply.github.com>
2024-06-07 09:02:29 +0200

    Placeholders and general code added for a calibration of all 4 sides, which is saddely enough impossible.
    The front of our testing object is both not reachable, and does have any scannable surfaces. (Beyond the thin profiles, which seem to need ^<2mm distance to be detected.)

--- Kalibratie/Calibration.py
+++ Kalibratie/Calibration.py
@@ -22,6 +22,10 @@ def CalculateOffsets(measurement_points_abcd,digital_middlepoint):
     B = measurement_points_abcd[1] 
     C = measurement_points_abcd[2] 
     D = measurement_points_abcd[3] 
+    E = measurement_points_abcd[4]
+    F = measurement_points_abcd[5]
+    G = measurement_points_abcd[6]
+    H = measurement_points_abcd[7]
 
     # See Mark's feedback, using lstsq to calculate the lines instead of manually.
     # Put the point data together in arrays.
@@ -29,44 +33,39 @@ def CalculateOffsets(measurement_points_abcd,digital_middlepoint):
     measurements_line2_x = [C[0],D[0]]
     measurements_line1_y = [A[1],B[1]]
     measurements_line2_y = [C[1],D[1]]
+    measurements_line3_x = [E[0],F[0]]
+    measurements_line3_y = [E[1],F[1]]
+    measurements_line4_x = [G[0],H[0]]
+    measurements_line4_y = [G[1],H[1]]
 
     # Put x points of line 1 and 2 in a vertical matrix, pairing each with a 1.
     A1 = np.vstack([measurements_line1_x,np.ones(len(measurements_line1_x))]).T
-    A2 = np.vstack([measurements_line2_x,np.ones(len(measurements_line2_y))]).T
+    A2 = np.vstack([measurements_line2_x,np.ones(len(measurements_line2_x))]).T
+    A3 = np.vstack([measurements_line3_x,np.ones(len(measurements_line3_x))]).T
+    A4 = np.vstack([measurements_line4_x,np.ones(len(measurements_line4_x))]).T
     m1,c1 = np.linalg.lstsq(A1,measurements_line1_y,rcond=None)[0]
     m2,c2 = np.linalg.lstsq(A2,measurements_line2_y,rcond=None)[0]
+    m3,c3 = np.linalg.lstsq(A3,measurements_line3_y,rcond=None)[0]
+    m4,c4 = np.linalg.lstsq(A4,measurements_line4_y,rcond=None)[0]
 
-    x_intersect = (c2-c1)/(m1-m2)
-    corner_point = [x_intersect,m1*x_intersect+c1]
+    x_intersect_1 = (c2-c1)/(m1-m2)
+    x_intersect_2 = (c3-c2)/(m2-m3)
+    x_intersect_3 = (c4-c3)/(m3-m4)
+    x_intersect_4 = (c1-c4)/(m4-m1)
+
+    corner_point_1 = [x_intersect_1,m1*x_intersect_1+c1]
+    corner_point_2 = [x_intersect_2,m2*x_intersect_2+c2]
+    corner_point_3 = [x_intersect_3,m3*x_intersect_3+c3]
+    corner_point_4 = [x_intersect_4,m4*x_intersect_1+c4]
 
     # Calculating the middle of the square through the previously calculated lines.
     # First step, along Line 1.
-    travel_length = SIDELENGTH/2
+    
+    centre_point = [(corner_point_1[0]+corner_point_2[0]+corner_point_3[0]+corner_point_4[0])/4,(corner_point_1[1]+corner_point_2[1]+corner_point_3[1]+corner_point_4[1])/4]
 
-    angle_1 = math.atan(m1)
-    if CORNER=="TopRight":
-        if angle_1<0:
-            delta_1 = [1*(math.cos(angle_1)*travel_length),1*(math.sin(angle_1)*travel_length)]
-        else:
-            delta_1 = [1*(math.cos(angle_1)*travel_length),-1*(math.sin(angle_1)*travel_length)]
-    else:
-        print("[ERROR] Corner not prepared yet.")
-        exit(1)
-    centre_point = [corner_point[0]+delta_1[0],corner_point[1]+delta_1[1]]
-
-    # 2nd step, along Line 2.
-    angle_2 = math.atan(m2)
-
-    if CORNER=="TopRight":
-        delta_2 =  [-1*(math.cos(angle_2)*travel_length),1*(math.sin(angle_2)*travel_length),10]
-    else:
-        print("[ERROR] Corner not prepared yet.")
-        exit(1)
-    centre_point = [round(centre_point[0]+delta_2[0],10),round(centre_point[1]+delta_2[1],10)]
-
-    A3 = np.vstack([[centre_point[0],corner_point[0]],np.ones(2)]).T
-    m3,c3 = np.linalg.lstsq(A3,[centre_point[1],corner_point[1]],rcond=None)[0]
-    diagonal = math.atan(m3)
+    DIAG = np.vstack([[centre_point[0],corner_point_1[0]],np.ones(2)]).T
+    DIAG_m,DIAG_c = np.linalg.lstsq(DIAG,[centre_point[1],corner_point_1[1]],rcond=None)[0]
+    diagonal = math.atan(DIAG_m)
     rz_offset = diagonal
 
     ## Debug prints for angle.
@@ -74,21 +73,6 @@ def CalculateOffsets(measurement_points_abcd,digital_middlepoint):
     # print(digital_middlepoint[5])
 
     # Output is X, Y and rZ in Radians. [X,Y,rZ]
-    offset_x_y_rz = [centre_point[0]-digital_middlepoint[0]+0.0032521844999999455,centre_point[1]-digital_middlepoint[1]+0.0015827895142857104,rz_offset-digital_middlepoint[5]]
+    offset_x_y_rz = [centre_point[0]-digital_middlepoint[0],centre_point[1]-digital_middlepoint[1],rz_offset-digital_middlepoint[5]]
 
-    # Debugging section
-    # print("--------Calibration Debug--------------")
-    # print("Travel: " + str(travel_length))
-    # print("CE1: " + str(coefficient_1) + ", CE2: " + str(coefficient_2))
-    # print("Offset 1: " + str(offset_1) + ", Offset 2: " + str(offset_2))
-    # print()
-    # print("Corner point: " + str(corner_point))
-    # print()
-    # print("Delta 1: " + str(delta_1) + ", Delta 2: " + str(delta_2))
-    # print("Angle_1: " + str(math.degrees(angle_1)) + ", Angle_2: " + str(math.degrees(angle_2)))
-    # print("Angle_1 Cos: " + str(math.cos(angle_1)) + ", Angle_2 Cos: " + str(math.cos(angle_2)))
-    # print("Angle_1 Sin: " + str(math.sin(angle_1)) + ", Angle_2 sin: " + str(math.sin(angle_2)))
-    # print("Offsets: " + str(offset_x_y_rz))
-    # print("--------End of Calibration Debug-------")
-    # print("\n")
     return offset_x_y_rz
--- Kalibratie/Main.py
+++ Kalibratie/Main.py
@@ -16,8 +16,21 @@ TCP = ["p[-0.211457, -0.001123, 0.100158, 0.000000, -1.570796, 0.000000]","p[-0.
 TCP_SCREWDRIVER = TCP[0]
 TCP_SENSOR = TCP[1]
 
-MeasurementPoses = ["[-0.690796,-1.46479,2.05802,-0.585906,-0.702562,0.0167929]","[-0.755371,-1.43441,2.02297,-0.581487,-0.767256,0.0174955]","[-0.70979,-0.491054,1.31468,2.25931,-0.844268,0.0379639]","[-0.680549,-0.44912,1.21117,2.32224,-0.873493,0.0359359]"]
-InbetweenPoses = ["[-1.0023,-1.04478,1.54754,-0.448062,-0.0243877,1.52694]"]
+MeasurementPoses = [
+    ["[-0.690796,-1.46479,2.05802,-0.585906,-0.702562,0.0167929]","[-0.755371,-1.43441,2.02297,-0.581487,-0.767256,0.0174955]"], #Back side.
+    ["[-0.70979,-0.491054,1.31468,2.25931,-0.844268,0.0379639]","[-0.680549,-0.44912,1.21117,2.32224,-0.873493,0.0359359]"], # Left side.
+    ["placeholder_1","placeholder_2"], # Front side.
+    ["placeholder_3","placeholder_4"] # Right side.
+    ]
+InbetweenPoses = [
+    "[-1.0023,-1.04478,1.54754,-0.448062,-0.0243877,1.52694]", # Back to left.
+    "placeholder_5", # Left to front.
+    "placeholder_6", # Front to right.
+    "placeholder_7", # Right to back.
+    ]
+
+# This is the value used to calculate the final offset.
+# Change this into a variable read from generated URScript, instructions for this should be inside other documentation.
 DigitalMiddlePoint = urc.poseToValues("p[-1.011832,0.047732,-0.0871,0.0,0.0,0.0]")
 
 
@@ -25,31 +38,35 @@ def DoCalibration(write_conn,read_conn):
     urc.syncWrite("movel("+InbetweenPoses[0]+")",write_conn,read_conn)
     urc.syncWrite("set_tcp("+ TCP_SCREWDRIVER +")",write_conn,read_conn)
 
-    urc.syncWrite("movel("+MeasurementPoses[0]+")",write_conn,read_conn)
+    urc.syncWrite("movel("+MeasurementPoses[0][0]+")",write_conn,read_conn)
     urc.syncWrite("set_tcp("+ TCP_SENSOR +")",write_conn,read_conn)
-    measurement_1 = st.MeasurePoint(write_conn,read_conn,0)
+    measurement_0_0 = st.MeasurePoint(write_conn,read_conn,0)
     urc.syncWrite("set_tcp("+ TCP_SCREWDRIVER +")",write_conn,read_conn)
 
 
     # Take measurement 2: High-Back 2.
-    urc.syncWrite("movej("+MeasurementPoses[1]+")",write_conn,read_conn)
+    urc.syncWrite("movej("+MeasurementPoses[0][1]+")",write_conn,read_conn)
     urc.syncWrite("set_tcp("+ TCP_SENSOR +")",write_conn,read_conn)
-    measurement_2 = st.MeasurePoint(write_conn,read_conn,1)
+    measurement_0_1 = st.MeasurePoint(write_conn,read_conn,1)
     urc.syncWrite("set_tcp("+ TCP_SCREWDRIVER +")",write_conn,read_conn)
 
     # Move through inbetween position.
     urc.syncWrite("movel("+InbetweenPoses[0]+")",write_conn,read_conn)
 
     # Take measurement 3: Low-Left.
-    urc.syncWrite("movel("+MeasurementPoses[2]+")",write_conn,read_conn)
+    urc.syncWrite("movel("+MeasurementPoses[1][0]+")",write_conn,read_conn)
     urc.syncWrite("set_tcp("+ TCP_SENSOR +")",write_conn,read_conn)
-    measurement_3 = st.MeasurePoint(write_conn,read_conn,2)
+    measurement_1_0 = st.MeasurePoint(write_conn,read_conn,2)
     urc.syncWrite("set_tcp("+ TCP_SCREWDRIVER +")",write_conn,read_conn) 
 
-    urc.syncWrite("movel("+MeasurementPoses[3]+")",write_conn,read_conn)
+    urc.syncWrite("movel("+MeasurementPoses[1][1]+")",write_conn,read_conn)
     urc.syncWrite("set_tcp("+ TCP_SENSOR +")",write_conn,read_conn)
-    measurement_4 = st.MeasurePoint(write_conn,read_conn,3)
+    measurement_1_1 = st.MeasurePoint(write_conn,read_conn,3)
     urc.syncWrite("set_tcp("+ TCP_SCREWDRIVER +")",write_conn,read_conn)
+
+    # WIP: Add 2 measurement positions for the front here.
+
+    # WIP: Add 2 measurement positions for the right here.
 
     # Returning back to inbetween position.
     urc.syncWrite("movel("+InbetweenPoses[0]+")",write_conn,read_conn)
@@ -60,16 +77,22 @@ def DoCalibration(write_conn,read_conn):
     # print(urc.poseToValues(measurement_3))
     # print("-----------------------")
 
+    measurements = [[measurement_0_0,measurement_0_1],[measurement_1_0,measurement_1_1]]
+
     measurement_1 = urc.poseToValues(measurement_1)
     measurement_2 = urc.poseToValues(measurement_2)
     measurement_3 = urc.poseToValues(measurement_3)
     measurement_4 = urc.poseToValues(measurement_4)
 
-    a = [float(measurement_1[0]),float(measurement_1[1]),float(measurement_1[5])]
-    b = [float(measurement_2[0]),float(measurement_2[1]),float(measurement_2[5])]
-    c = [float(measurement_3[0]),float(measurement_3[1]),float(measurement_3[5])]
-    d = [float(measurement_4[0]),float(measurement_4[1]),float(measurement_4[5])]
-    out = cal.CalculateOffsets([a,b,c,d],DigitalMiddlePoint)
+    a = [float(measurements[0][0][0]),float(measurement_1[0][0][1]),float(measurement_1[0][0][5])]
+    b = [float(measurements[0][1][0]),float(measurements[0][1][1]),float(measurements[0][1][5])]
+    c = [float(measurements[1][0][0]),float(measurements[1][0][1]),float(measurements[1][0][5])]
+    d = [float(measurements[1][1][0]),float(measurements[1][1][1]),float(measurements[1][1][5])]
+    e = [float(measurements[2][0][0]),float(measurements[2][0][1]),float(measurements[2][0][5])]
+    f = [float(measurements[2][1][0]),float(measurements[2][1][1]),float(measurements[2][1][5])]
+    g = [float(measurements[3][0][0]),float(measurements[3][0][1]),float(measurements[3][0][5])]
+    h = [float(measurements[3][1][0]),float(measurements[3][1][1]),float(measurements[3][1][5])]
+    out = cal.CalculateOffsets([a,b,c,d,e,f,g,h],DigitalMiddlePoint)
     # print("Debug: " + str(out[0]) + ", " + str(out[1]) + ", " + str(math.degrees(out[2])))
     return [out[0],out[1],out[2]]
 
